829ca4e7856b853d4ea4677cf7143014a0c75cc6
Nishan Bajracharya <nisbaj11@gmail.com>
2021-10-04 10:33:08 +0545

    Add ssr support

---
+++ src/Counter.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </>
+  );
+}
+
+export default Counter;
